904d558bb5c71e4f57cd04a04ba7ae910d098270
Zenology <zen.noppanan@gmail.com>
2025-11-16 18:21:25 +0700

    feat: add auto move v.0.0.1

---
+++ src/lua/game-script/auto-move.lua
@@ -0,0 +1,24 @@
+--[=====[
+[[SND Metadata]]
+author: 'Zenology'
+version: 0.0.1
+description: Auto Move - Script for Auto Move
+plugin_dependencies:
+- vnavmesh
+[[End Metadata]]
+--]=====]
+
+local mountCondition = 4
+
+if IPC.vnavmesh.IsReady() then
+	if Player.CanFly then
+		yield("/vnav flyflag")
+	else
+		yield("/vnav moveflag")
+	end
+end
+
+if not Svc.Condition[mountCondition] then
+	-- Send Page Down button
+	yield("/send NEXT")
+end
